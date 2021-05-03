b7df9c480810da1a95057ef2025a1a20cacf40a1
j-takamoto <jtakamoto@mtc-sams.net>
2021-05-03 15:32:11 +0900

    サンプルソース

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/test')
+def test():
+    return render_template('products.html', title='作品情報')
